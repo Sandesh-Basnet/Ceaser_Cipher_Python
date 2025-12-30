dddb1c7bad6b7abcafa6315065ba77b1ce447fd9
Sandesh Basnet <dybalafan.sandesh@gmail.com>
2025-12-30 21:09:25 +0545

    Welcome Function

---
+++ ceaser_cipher_sandesh_basnet_2603784.py
@@ -0,0 +1,6 @@
+def welcome():# Function to display a welcome message
+    print("Welcome to the Caesar Cipher Program!")
+    print("This program allows you to encrypt and decrypt messages using the Caesar cipher technique.")
+    print("You can choose to shift letters by a specified number to encode or decode your messages.")
+    print("Let's get started!")
+welcome()
